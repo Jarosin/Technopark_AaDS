c4e702f2ec3482c4a975821b4e151511551664c9
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-09 21:08:55 +0300

    made first version

--- module1/task1/main.cpp
+++ module1/task1/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+
+int find_bit(int n, int k)
+{
+    return (n >> k) & 1;
+}
+
 int main()
 {
+    unsigned long int n;
+    int k;
+    std::cin >> n >> k;
+    std::cout << find_bit(n, k);
     return 0;
 }
