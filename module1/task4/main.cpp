c7cdccaac776340e3470914ddf8e6379d3884667
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-21 01:26:37 +0300

    finish task4

--- module1/task4/main.cpp
+++ module1/task4/main.cpp
@@ -4,68 +4,91 @@
 Формат входных данных: Сначала вводится количество массивов K. Затем по очереди размер каждого массива и элементы массива. Каждый массив упорядочен по возрастанию.
 Формат выходных данных: Итоговый отсортированный массив.*/
 #include <iostream>
-//TODO: сделать код безопасным
+
 template <typename T>
+struct DefaultComparator {
+    bool operator()(const T& l, const T& r) const {
+        return l < r;
+    }
+};
+template <typename T, typename Comparator = DefaultComparator<T>>
 class Heap
 {
     public:
-        Heap(int k) 
+        Heap(int k, Comparator cmp = Comparator())
         {
+            this->cmp = cmp;
             len = 0;
             buf = new T[k];
+            capacity = k;
         };
         ~Heap() 
         {
             delete[] buf;
         };
-        void Insert(T element, int (*cmp)(T, T))
+        void push(T element)
         {
             Append(element);
-            siftUp(len - 1, cmp);
+            siftUp(len - 1);
         }
-        T ExtractMax(int (*cmp)(T, T))
+        void pop()
         {
             len--;
-            T max = buf[0];
             buf[0] = buf[len];
-            siftDown(0, cmp);
-            return max;
+            siftDown(0);
         }
-        T PeekMax()
+        const T& top()
         {
             return buf[0];
         }
     private:
         T *buf;
         int len;
-        void siftDown(int i, int (*cmp)(T, T))
+        int capacity;
+        Comparator cmp;
+        void grow()
+        {
+            T *new_buf = new T[capacity * 2];
+            T *fin = new_buf + len;
+            for (auto it = new_buf; it < fin; it++)
+            {
+                *it = top();
+                pop();
+            }
+            delete[] buf;
+            capacity *= 2;
+            buf = new_buf;
+        }
+        void siftDown(int i)
         {
             int largest = i;
             int left = 2 * i + 1;
             int right = 2 * i + 2;
-            if (left < len && cmp(buf[left], buf[largest]) > 0)
+            if (left < len && !cmp(buf[left], buf[largest]))
                 largest = left;
-            if (right < len && cmp(buf[right], buf[largest]) > 0)
+            if (right < len && !cmp(buf[right], buf[largest]))
                 largest = right;
             if (largest != i)
             {
                 std::swap(buf[largest], buf[i]);
-                siftDown(largest, cmp);
+                siftDown(largest);
             }
         }
-        void siftUp(int i, int (*cmp)(T, T))
+        void siftUp(int i)
         {
             if (i == 0)
                 return;
             int parent = (i - 1) / 2;
-            if (cmp(buf[parent], buf[i]) < 0)
+            if (cmp(buf[parent], buf[i]))
             {
                 std::swap(buf[i], buf[parent]);
-                siftUp(parent, cmp);
+                siftUp(parent);
             }
         }
         void Append(T new_el)
         {
+            if (len > capacity)
+                grow();
             buf[len] = new_el;
             len++;
         }
@@ -84,16 +107,19 @@ class Capsule
         }
         Capsule(){};
         ~Capsule(){};
+        bool operator<(const Capsule& r) const
+        {
+            return data < r.data;
+        }
 };
 template <typename T>
 int CompareCapsules(Capsule<T> a, Capsule<T> b)
 {
     return a.data - b.data;
 }
-//два варианта: либо запоминать номер массива с самим элементом(оберкта), либо создать массив, хранящий последний изъятый элемент
-//Проблема массива отслеживания - при повторении одного элемента в нескольких массивах будут проблемы
+
 template <typename T>
-int MergeArrays(T **aa, int *lens, int k, int n, int *res, int (*cmp)(T, T))
+int MergeArrays(T **matrix, int *lens, int k, int n, int *res)
 {
     Heap<Capsule<T>> solver(k);
     for (int i = 0; i < k; i++)
@@ -101,22 +127,23 @@ int MergeArrays(T **aa, int *lens, int k, int n, int *res, int (*cmp)(T, T))
         if (lens[i] > 0)
         {
             lens[i]--;
-            Capsule<T> temp(aa[i][lens[i]], i);
-            solver.Insert(temp, &CompareCapsules);
+            Capsule<T> temp(matrix[i][lens[i]], i);
+            solver.push(temp);
         }
     }
     int cur = n - 1;
     for (int i = 0; i < n; i++)
     {
-        Capsule<T> temp = solver.ExtractMax(&CompareCapsules);
+        Capsule<T> temp = solver.top();
+        solver.pop();
         res[cur] = temp.data;
         cur--;
         int arr_number = temp.arr_number;
         if (lens[arr_number] > 0)
         {
             lens[arr_number]--;
-            Capsule<T> temp(aa[arr_number][lens[arr_number]], arr_number);
-            solver.Insert(temp, &CompareCapsules);
+            Capsule<T> temp(matrix[arr_number][lens[arr_number]], arr_number);
+            solver.push(temp);
         }
     }
     return 0;
@@ -128,47 +155,35 @@ int compare(int a, int b)
 }
 int main()
 {
-    int **aa;
+    int **matrix;
     int rows;
     int *lengths;
     int n = 0;
     std::cin >> rows;
-    aa = new int*[rows];
+    matrix = new int*[rows];
     lengths = new int[rows];
     for (int i = 0; i < rows; i++)
     {
         int columns = 0;
         std::cin >> columns; 
-        aa[i] = new int[columns];
+        matrix[i] = new int[columns];
         lengths[i] = columns;
         n += columns;
         for (int j = 0; j < columns; j++)
         {
-            std::cin >> aa[i][j];
+            std::cin >> matrix[i][j];
         }
     }
     int *res = new int[n];  
-    MergeArrays<int>(aa, lengths, rows, n, res, &compare);
+    MergeArrays<int>(matrix, lengths, rows, n, res);
     for (int i = 0; i < n; i++)
         std::cout << res[i] << " ";
     delete[] res;
     for (int i = 0; i < rows; i++)
     {
-        delete[] aa[i];
+        delete[] matrix[i];
     }
-    delete[] aa;
+    delete[] matrix;
     delete[] lengths;
     return 0;
 }
-
-/*5
-3
-1 2 3
-3
-4 5 6
-3
-7 8 9
-3
-10 11 12
-3
-13 14 15*/
