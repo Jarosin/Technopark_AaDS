b7b7051f2a6aa90eea8ec2c6bd079aa30bf073c5
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-26 23:04:57 +0300

    finish task 7

--- module1/task7/main.cpp
+++ module1/task7/main.cpp
@@ -2,7 +2,8 @@
 Дан массив неотрицательных целых 64-разрядных чисел. Количество чисел не больше 10^6. Отсортировать массив методом MSD по битам (бинарный QuickSort).
 */
 #include <iostream>
-int find_bit(int n, int k)
+#include <cassert>
+int find_bit(unsigned long long n, int k)
 {
     return (n >> k) & 1;
 }
@@ -14,8 +15,8 @@ int BinaryQuickSort(unsigned long long *start, unsigned long long *end, int bit)
     unsigned long long *j = end;
     while (i < j)
     {
-        for (; i < end && !find_bit(*i, bit); i++) {}
-        for (; j > start && find_bit(*j, bit); j--) {}
+        for (; i < end && !find_bit(*i, bit); i++) continue;
+        for (; j > start && find_bit(*j, bit); j--) continue;
         if (i < j) {
             std::swap(*i, *j);
         }
@@ -26,21 +27,84 @@ int BinaryQuickSort(unsigned long long *start, unsigned long long *end, int bit)
         BinaryQuickSort(i, end, bit - 1);
     return 0;
 }
+template <typename T>
+int check_sorted(T *arr, int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            std::cout << "Didnt sort properly" << std::endl;
+            return 1;
+        }
+    }
+    std::cout << "It's ok" << std::endl;
+    return 0;
+}
+
+int test()
+{
+    int bit_count = 63;
+    int rc = 0;
+
+    unsigned long long arr1[] = {1, 2, 3, 4};
+    int len = 4;
+    BinaryQuickSort(arr1, arr1 + len - 1, bit_count);
+    if (!rc)
+        rc = check_sorted(arr1, len);
+
+    len = 100000;
+    unsigned long long *arr2 = new unsigned long long[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr2[len - i - 1] = i;
+    }
+    BinaryQuickSort(arr2, arr2 + len - 1, bit_count);
+    if (!rc)
+        rc = check_sorted(arr2, len);
+    delete[] arr2;
+    
+    len = 64;
+    arr2 = new unsigned long long[len];
+    unsigned long long a = 1;
+    for (int i = 0; i < len; i++)
+    {
+        arr2[len - i - 1] = a;
+        std::cout << a << " ";
+        a *= 2;
+    }
+    BinaryQuickSort(arr2, arr2 + len - 1, bit_count);
+    if (!rc)
+        rc = check_sorted(arr2, len);
+    delete[] arr2;
+
+    return rc;
+}
 
 int main()
-{
+{   
+    //assert(!test());
     int n = 0;
     std::cin >> n;
+    unsigned long long max = 0;
     unsigned long long *arr = new unsigned long long[n];
     for (int i = 0; i < n; i++)
     {
-        std::cin >> arr[i];    
+        std::cin >> arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }  
+    int bit_number = 0;
+    for (int i = 0; i < 63; i++)
+    {
+        if ((max >> i) & 1)
+            bit_number = i;   
     }
-    int bit_number = 63;
     BinaryQuickSort(arr, arr + n - 1, bit_number);
     for (int i = 0; i < n; i++)
     {
         std::cout << arr[i] << " ";
     }
+    delete[] arr;
     return 0;
 }
