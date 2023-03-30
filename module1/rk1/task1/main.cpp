5b2d517c0c6e16228b3df39c02ee135310183ebe
Spirin Mihail <misha_spirin_90@mail.ru>
2023-03-30 19:11:52 +0300

    finish rk1

--- module1/rk1/task1/main.cpp
+++ module1/rk1/task1/main.cpp
@@ -1,5 +1,54 @@
+/*
+Дан отсортированный по возрастанию массив попарно различных целых чисел A[0..n-1]. На вход также подается некоторое целое число k. Необходимо вернуть индекс элемента k в массиве A, если он там присутствует, либо вернуть позицию, куда его следовало бы вставить, чтобы массив остался отсортированным.
+
+Требование: сложность O(logN)
+
+Формат ввода
+В первой строчке записано число n – размерность массива A. Во второй строчке перечислены элементы массива A. В третьей строчке записано значение k.
+
+Формат вывода
+Целое число — позиция k в A.
+
+Ввод
+4
+1 3 5 6
+5
+Вывод:
+2
+*/
 #include <iostream>
+int BinarySearch(int *arr, int len, int k)
+{
+    int *start = arr;
+    int *end = arr + len - 1;
+    while (start < end)
+    {
+        int *middle = (end - start) / 2 + start;
+        if (k == *middle)
+            return middle - arr;
+        if (k < *middle)
+        {
+            end = middle - 1;
+        }
+        else
+        {
+            start = middle + 1;
+        }
+    }
+    if (k > *start)
+        return start - arr + 1;
+    return start - arr;
+}
 int main()
 {
+    int n = 0;
+    std::cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+        std::cin >> arr[i];
+    int k = 0;
+    std::cin >> k;
+    int pos = BinarySearch(arr, n, k);
+    std::cout << pos;    
     return 0;
 }
--- module1/rk1/task2/main.cpp
+++ module1/rk1/task2/main.cpp
@@ -1,5 +1,102 @@
+/*
+ВНИМАНИЕ! Для решения данной задачи нужно использовать структуру данных стек!
+
+Пара слов называется стековой анаграмой, если одно слово можно получить из другого, проведя последовательность стековых операций с его буквами (взять очередную букву исходного слова и поместить ее в стек; взять букву из стека и добавить ее в конец выходного слова). Для заданной пары слов требуется определить, можно ли выполнить последовательность стековых операций, переводящую первое слово во второе. Все буквы в слове различные. Длина анаграммы ≤ 10000.
+
+Формат ввода
+Пара слов, являющихся анаграммой.
+
+Формат вывода
+YES, если последовательность стековых операций существует и NO в противном случае.
+
+Ввод
+STOL SLOT 
+Вывод:
+YES
+
+Ввод
+ABC CAB
+Вывод:
+NO
+*/
 #include <iostream>
+#include <string>
+template<typename T>
+class Stack
+{
+    public:
+    Stack(int len)
+    {
+        buf_ = new T[len];
+        end_ = buf_ + len;
+        cur_ = buf_;
+    }
+    void push(T item)
+    {
+        if (cur_ > end_)
+            return;
+        *cur_ = item;
+        cur_++;
+    }
+    T pop()
+    {
+        cur_--;
+        T res = *cur_;
+        return res;
+    }
+    T top()
+    {
+        T res;
+        if (empty())
+            return res;
+        return *(cur_ - 1);
+    }
+    bool empty()
+    {
+        return cur_ == buf_;
+    }
+    ~Stack()
+    {
+        delete[] buf_;
+    }
+    private:
+    T *end_;
+    T *buf_;
+    T *cur_;
+};
+
+int stack_anagram(std::string first, std::string second)
+{
+    Stack<char> st(first.size());
+    std::string res = "";
+    int j = 0, i = 0;
+    while (i < second.size())
+    {
+        if (!st.empty() && second[i] == st.top())
+        {
+            res += st.pop();
+            i++;
+        }
+        else if (j < first.size())
+        {
+            st.push(first[j]);
+            j++;
+        }
+        else if (!st.empty())
+        {
+            res = res + st.pop();
+        }
+        else
+            break;
+    }
+    return res == second;
+}
 int main()
 {
+    std::string first;
+    std::string second;
+    std::cin >> first >> second;
+    std::string ans = stack_anagram(first, second) ? "YES" : "NO";
+    std::cout << ans;
     return 0;
 }
